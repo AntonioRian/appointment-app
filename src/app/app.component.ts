854229a7168b221961203ea22283252927ab1f83
AntonioRian <rian.crato@gmail.com>
2025-08-26 23:06:34 -0300

    feat: adicionado estrutura base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppointmentListComponent } from './appointment-list/appointment-list.component';
+import { FormsModule } from '@angular/forms';
+import { AppointmentBoardComponent } from "./appointment-board/appointment-board.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    AppointmentListComponent,
+    FormsModule,
+    AppointmentBoardComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
